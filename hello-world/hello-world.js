f2baafab90bd8b37312bd8757f982797184416f7
evgenii <evgenii@methodstudios.co.nz>
2017-03-06 16:53:49 +1300

    hello-world exercise

---
+++ hello-world/hello-world.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class HelloWorld {
+  hello(name) {
+    name = name || "World";
+    let hello = "Hello, ";
+    return hello + name + "!";
+  }
+}
+
+export default HelloWorld;
+
